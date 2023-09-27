b17e1d8e666e93f647444132780c3e23efed722d
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-09-27 22:45:48 +0700

    i

--- install-cupy.py
+++ install-cupy.py
@@ -15,16 +15,21 @@ def get_cuda_ver(nvrtc):
         return '12x'
     return None
 
-cuda_ver = None
-if "CUDA_HOME" not in os.environ:
-    import torch
-    torch_lib_path = Path(torch.__file__).parent / "lib"
-    torch_lib_path = str(torch_lib_path.resolve())
-    if os.path.exists(torch_lib_path):
-        nvrtc = filter(lambda lib_file: "nvrtc-builtins" in lib_file, os.listdir(torch_lib_path))
-        nvrtc = list(nvrtc)[0]
-        cuda_ver = get_cuda_ver(nvrtc)
+try:
+    import cupy
+    print("CuPy is already installed.")
+except:
+    cuda_ver = None
+    cuda_ver = None
+    if "CUDA_HOME" not in os.environ:
+        import torch
+        torch_lib_path = Path(torch.__file__).parent / "lib"
+        torch_lib_path = str(torch_lib_path.resolve())
+        if os.path.exists(torch_lib_path):
+            nvrtc = filter(lambda lib_file: "nvrtc-builtins" in lib_file, os.listdir(torch_lib_path))
+            nvrtc = list(nvrtc)[0]
+            cuda_ver = get_cuda_ver(nvrtc)
 
-s_param = '-s' if "python_embeded" in sys.executable else '' 
-cupy_package = f"cupy-cuda{cuda_ver}" if cuda_ver is not None else "cupy-wheel"
-os.system(f'"{sys.executable}" {s_param} -m pip install {cupy_package}')
+    s_param = '-s' if "python_embeded" in sys.executable else '' 
+    cupy_package = f"cupy-cuda{cuda_ver}" if cuda_ver is not None else "cupy-wheel"
+    os.system(f'"{sys.executable}" {s_param} -m pip install {cupy_package}')
