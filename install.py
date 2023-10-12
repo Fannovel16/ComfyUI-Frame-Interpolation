31e6f6b8015afe5e844b2b70063a6d097f28558c
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-10-12 18:31:54 +0700

    Include nvrtc 11.8 for Windows (https://github.com/cupy/cupy/issues/7776)

--- install.py
+++ install.py
@@ -50,11 +50,11 @@ def install_cupy():
         cupy_package = f"cupy-cuda{cuda_ver}" if cuda_ver is not None else "cupy-wheel"
         os.system(f'"{sys.executable}" {s_param} -m pip install {cupy_package}')
 
-""" with open(Path(__file__).parent / "requirements-no-cupy.txt", 'r') as f:
+with open(Path(__file__).parent / "requirements-no-cupy.txt", 'r') as f:
     for package in f.readlines():
         package = package.strip()
         print(f"Installing {package}...")
-        os.system(f'"{sys.executable}" {s_param} -m pip install {package}') """
+        os.system(f'"{sys.executable}" {s_param} -m pip install {package}')
 
 print("Checking cupy...")
 install_cupy()
